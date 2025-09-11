01a2756dc42a149a7a98115c474ced9605a3e2d9
Gildácio Lopes <gildacioj399@gmail.com>
2025-09-11 07:46:29 -0300

    refactor: update footer copyright text and remove developer attribution for rebranding

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,14 +1,9 @@
 const Footer = () => {
   return (
     <div className="bg-accent w-full gap-1 p-8">
-      <p className="text-xs font-medium">© 2025 Copyright BEWEAR</p>
+      <p className="text-xs font-medium">© 2025 Copyright The Grife</p>
       <p className="text-muted-foreground text-xs font-medium">
-        Desenvolvido por{" "}
-        <span className="font-bold text-[#8162FF] hover:cursor-pointer">
-          <a href="https://gildaciolopes.netlify.app/" target="_blank">
-            Gildácio Lopes
-          </a>
-        </span>
+        Todos os direitos reservados.
       </p>
     </div>
   );
